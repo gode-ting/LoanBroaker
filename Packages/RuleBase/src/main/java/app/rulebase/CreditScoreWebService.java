535eb097fc53d2828767a9a0fb913e6414c5c370
Daniel Hillmann <hiller@live.dk>
2017-09-25 22:41:07 +0200

    Updates GetBanks. Updates RuleBase WebService

--- Packages/RuleBase/src/main/java/app/rulebase/CreditScoreWebService.java
+++ Packages/RuleBase/src/main/java/app/rulebase/CreditScoreWebService.java
@@ -4,17 +4,12 @@ import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 
-@WebService(serviceName = "CreditScoreWebService")
+@WebService(serviceName = "GetBankWebServices")
 public class CreditScoreWebService {
 
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
-    }
+    @WebMethod(operationName = "getBankRules")
+    public String getCreditScore(@WebParam(name = "snn") String ssn) {
 
-    @WebMethod(operationName = "getCreditScore")
-    public String getCreditScore(@WebParam(name = "snn") int snn) {
-
-        return "ssn";
+        return ssn;
     }
 }
