f79ffb07761ffd22d9c07de1c6dfe52411d3189c
Daniel Hillmann <hiller@live.dk>
2017-10-07 09:22:37 +0200

    Add translator

---
+++ Packages/TranslatorJson/test/translator.test.js
@@ -0,0 +1,11 @@
+let expect = require('chai').expect;
+
+let translator = require('../src/translator.js');
+
+describe('../src/transaltor.js', () => {
+	describe('', () => {
+		it('', () => {
+
+		});
+	});
+});
