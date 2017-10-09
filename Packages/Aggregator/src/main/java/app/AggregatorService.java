8f548375a2257195e58236f0a6cc5eb7cad9c39b
lalelarsen <frederikblarsen@hotmail.com>
2017-10-09 20:47:05 +0200

    fixing the aggregator

--- Packages/Aggregator/src/main/java/app/AggregatorService.java
+++ Packages/Aggregator/src/main/java/app/AggregatorService.java
@@ -29,18 +29,37 @@ public class AggregatorService {
             public void run() {
                 try {
                     if (message.containsKey("banks")) {
+                        System.out.println("---- New application added waiting queue----");
+                        System.out.println("");
+                        System.out.println("SSN: " + (String)((HashMap)message.get("application")).get("ssn"));
+                        System.out.println("Waiting for " + ((ArrayList) message.get("banks")).size() + " banks...");
+                        System.out.println("");
                         if (((ArrayList) message.get("banks")).size() < 1) {
                             throw new Exception();
                         }
                         unfinishedMessages.put((String)((HashMap)message.get("application")).get("ssn"),new messageCollection(((ArrayList) message.get("banks")).size(), (String)((HashMap)message.get("application")).get("ssn")));
                     } else {
-                        messageCollection mc = unfinishedMessages.get((String)((HashMap)message.get("application")).get("ssn"));
-                        mc.getMessages().add(message);
-                        System.out.println("waiting for: " + mc.getSize() + "   has: " + mc.getMessages().size());
+                        messageCollection mc = unfinishedMessages.get((String)message.get("ssn"));
+                        System.out.println("." + mc);
+                        System.out.println("." + mc.getMessages().size());
+                        System.out.println("." + message);
+                        mc.getMessages().put("test" + mc.getMessages().size(), message);
+                        System.out.println("---- New message from bank ----");
+                        System.out.println("");
+                        System.out.println("SSN: " + (String)message.get("ssn"));
+                        System.out.println("Message no. " + mc.getMessages().size() + "/" + mc.getSize());
+                        System.out.println("");
+                        if(mc.getSize() == mc.getMessages().size()){
+                            System.out.println("---- Message send to user ----");
+                            System.out.println("SSN: " + (String)message.get("ssn"));
+                            delegate.didAggregatorServiceWithOptionalException(mc.getMessages(), null);
+                            unfinishedMessages.remove((String)message.get("ssn"));
+                        }
                     }
-                    delegate.didAggregatorServiceWithOptionalException(message, null);
+                    
 
                 } catch (Exception e) {
+                    e.printStackTrace();
                     delegate.didAggregatorServiceWithOptionalException(null, e);
                 }
             }
@@ -51,12 +70,12 @@ public class AggregatorService {
     class messageCollection{
         int size;
         String ssn;
-        ArrayList<HashMap> messages;
+        HashMap messages;
 
         public messageCollection(int size, String ssn) {
             this.size = size;
             this.ssn = ssn;
-            this.messages = new ArrayList<>();
+            this.messages = new HashMap();
         }
 
         public int getSize() {
@@ -67,7 +86,7 @@ public class AggregatorService {
             return ssn;
         }
 
-        public ArrayList<HashMap> getMessages() {
+        public HashMap getMessages() {
             return messages;
         }
         
