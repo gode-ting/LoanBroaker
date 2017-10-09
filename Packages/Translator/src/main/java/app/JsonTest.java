480b831f897b34253ed8b9368598279ebdeb686c
Daniel Hillmann <hiller@live.dk>
2017-10-09 20:30:37 +0200

    add more json example

--- Packages/Translator/src/main/java/app/JsonTest.java
+++ Packages/Translator/src/main/java/app/JsonTest.java
@@ -1,24 +1,35 @@
 package app;
 
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class JsonTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         JSONObject response = new JSONObject();
         response.put("ssn", "xxxxxxx-xxxx");
         response.put("creditScore", 550);
         response.put("loanAmount", 1000.1);
         response.put("loanDuration", "360");
-        
-        System.out.println("response: " + response);
-        
+
+        String jsonString = "{\"loanDuration\":\"360\",\"creditScore\":550,\"loanAmount\":1000.1,\"ssn\":\"xxxxxxx-xxxx\"}";
+
+        System.out.println("response: " + response.toString());
+
         // Get one property - prints: SSN: xxxxxxx-xxxx
         System.out.println("SSN: " + response.get("ssn"));
-            
+
+        // Save to a variable and print - same print as above.
         String ssn = response.get("ssn").toString();
-        
         System.out.println("SSN: " + ssn);
+
+        JSONParser parser = new JSONParser();
+        JSONObject json = (JSONObject) parser.parse(jsonString);
         
+        System.out.println("Parsed json object: " + json);
+        
+        System.out.println("Get parsed json ssn: " + json.get("ssn"));
+
     }
 }
