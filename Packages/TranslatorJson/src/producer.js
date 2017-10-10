a64f51d39c556857b37b5d93d16f7fae13a6e81a
Daniel Hillmann <hiller@live.dk>
2017-10-10 20:15:51 +0200

    Refactor. Add tests. Remove console.logs

--- Packages/TranslatorJson/src/producer.js
+++ Packages/TranslatorJson/src/producer.js
@@ -1,4 +1,5 @@
 import rabbitmq from '../config/rabbitmq.js';
+import translator from './translator';
 
 export default function (ampqConn, message) {
 
@@ -12,7 +13,9 @@ export default function (ampqConn, message) {
 
 		let type = rabbitmq.producer.type;
 		let exchange = rabbitmq.producer.exchange;
-		let headers = {type: 'json'};
+		let headers = {
+			type: 'json'
+		};
 
 		ch.assertExchange(exchange, type, {
 			durable: false
@@ -20,25 +23,13 @@ export default function (ampqConn, message) {
 
 		//	Example of a message correctly formatted and ready to be send to cphbusiness.bankjson
 		//var newMessage = {'ssn':1605789787, 'creditScore':749, 'loanAmount': 10.0, 'loanDuration': 360};
-		let obj = JSON.parse(message.content.toString());
-		var formattedObject = {};
-		formattedObject.ssn = obj.ssn;
-		formattedObject.creditScore = obj.creditScore;
-		formattedObject.loanAmount = obj.loanAmount;
-		formattedObject.loanDuration = 360; // Dette skal der laves noget arbejde på. Skal laves om fra Date til antal dage
-		
+		let jsonObject = JSON.parse(message.content.toString());
+		let formattedObject = translator.getFormattedJson(jsonObject);
+
 		ch.publish(exchange, '', Buffer.from(JSON.stringify(formattedObject)), {
 			headers: headers,
 			replyTo: replyTo
 		});
 		console.log('Successfully sent message');
 	});
-	// setTimeout(() => {
-	// 	ampqConn.close();
-	// 	console.log('Conn closed');
-	// }, 500);
-}
-
-function translateJson () {
-
 }
