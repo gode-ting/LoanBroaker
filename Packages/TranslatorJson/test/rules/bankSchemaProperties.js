aec2e88640622c3071f0412337d47d16b1a82972
Daniel Hillmann <hiller@live.dk>
2017-10-10 21:22:52 +0200

    format

--- Packages/TranslatorJson/test/rules/bankSchemaProperties.js
+++ Packages/TranslatorJson/test/rules/bankSchemaProperties.js
@@ -1,46 +1,47 @@
-module.exports = [{
+module.exports = [
+	{
 		input: {
-			ssn: "1992-28-04",
-			creditScore: "850",
+			ssn: '1992-28-04',
+			creditScore: '850',
 			loanAmount: 100.1,
-			loanDuration: "350"
+			loanDuration: '350'
 		},
 		output: true
 	},
 	{
 		input: {
-			ssn: "2000-12-12",
-			creditScore: "100",
+			ssn: '2000-12-12',
+			creditScore: '100',
 			loanAmount: 200,
-			loanDuration: ""
+			loanDuration: ''
 		},
 		output: true
 	},
 	{
 		input: {
-			ssn: "2010-30-11",
-			creditScore: "500",
-			loanAmount: "100",
-			loanDuration: "100"
+			ssn: '2010-30-11',
+			creditScore: '500',
+			loanAmount: '100',
+			loanDuration: '100'
 		},
 		output: false
 	},
 	{
 		input: {
-			ssn: 2010-30-11,
-			creditScore: "100",
+			ssn: 2010 - 30 - 11,
+			creditScore: '100',
 			loanAmount: 100,
-			loanDuration: "500"
+			loanDuration: '500'
 		},
 		output: false
 	},
 	{
 		input: {
-			ssn: "2013-19-12",
+			ssn: '2013-19-12',
 			creditScore: 100,
 			loanAmount: 200,
-			loanDuration: "250"
+			loanDuration: '250'
 		},
 		output: false
 	}
-]
+];
